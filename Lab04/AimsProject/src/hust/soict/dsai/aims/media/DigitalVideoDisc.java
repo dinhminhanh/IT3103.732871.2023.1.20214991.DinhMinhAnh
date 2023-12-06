4bf4e6d5e826eb7d2572862639a81d20c2565caa
Anh Dinh <anh259743@gmail.com>
2023-12-06 22:44:10 +0700

    Finish Lab04

---
+++ Lab04/AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -0,0 +1,29 @@
+
+package hust.soict.dsai.aims.media;
+
+public class DigitalVideoDisc extends Disc implements Playable {
+    public DigitalVideoDisc(String director, int length) {
+
+        super(director, length);
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        super(title, category, director, length, cost);
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+
+        super(title, category, cost);
+    }
+    public DigitalVideoDisc(String title) {
+
+        super(title);
+    }
+    public DigitalVideoDisc() {
+        super();
+    }
+    public void play() {
+        System.out.println("Playing DVD: " + this.getTitle());
+        System.out.println("DVD length: " + this.getLength());
+    }
+}
